f3ad37e495686d22a7fbae2fb1431924cfc0a195
Ayaad Chouhan <136963590+AyaadChouhan@users.noreply.github.com>
2024-07-18 23:57:50 +0530

    Add files via upload

---
+++ Very-Easy-Question/Question-60.js
@@ -0,0 +1,28 @@
+// Arithmetic Progression
+// In mathematics, an Arithmetic Progression (AP) is a sequence of numbers such that the difference between the consecutive terms is constant. Create a function that takes three arguments:
+
+// First element of the sequence first
+// Constant difference between the elements diff
+// Total numbers in the sequence n
+// Return the first n elements of the sequence with the given common difference diff. The final result should be a string of numbers, separated by comma and space.
+
+// Examples
+// arithmeticProgression(1, 2, 5) ➞ "1, 3, 5, 7, 9"
+
+// arithmeticProgression(1, 0, 5) ➞ "1, 1, 1, 1, 1"
+
+// arithmeticProgression(1, -3, 10) ➞ "1, -2, -5, -8, -11, -14, -17, -20, -23, -26"
+
+function arithmeticProgression(n1,n2, n3) {
+    // let newArr = []
+    // let num = n1
+    // for (let i = 0; i < n3; i++) {
+    //     num +=  n2
+    //     newArr.push(num);
+    // }
+    // return String(newArr)
+    
+}
+console.log(arithmeticProgression(1, -3, 10));
+console.log(arithmeticProgression(1, 0, 5));
+console.log(arithmeticProgression(1, 2, 5));
