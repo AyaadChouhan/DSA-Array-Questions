b8946e6aaf7c5227a4e739ccf42c8169474d9146
Ayaad Chouhan <136963590+AyaadChouhan@users.noreply.github.com>
2024-08-18 00:20:04 +0530

    Add files via upload

---
+++ Very-Easy-Question/Question-118.js
@@ -0,0 +1,21 @@
+// Basic Statistics: Mean
+// The mean of a group of numbers is calculated by summing all numbers, and dividing this sum by the total count of numbers in the group. Given a sorted array of numbers, return the mean (rounded to one decimal place).
+
+// Examples
+// mean([1, 6, 6, 7, 8, 8, 9, 10, 10]) ➞ 7.2
+
+// mean([1, 3, 8, 9, 9, 10]) ➞ 6.7
+
+// mean([2, 3, 3, 6, 6, 8, 9, 10]) ➞ 5.9
+function mean(arr){
+    let sum = 0, count = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//          sum += arr[i];
+//          count++   
+//     }
+//    return (sum / count).toFixed(1)
+
+
+}
+console.log(mean([2, 3, 3, 6, 6, 8, 9, 10]))
+console.log(mean([1, 3, 8, 9, 9, 10]))
