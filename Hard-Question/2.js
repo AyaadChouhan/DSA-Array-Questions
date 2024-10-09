99f2dd37d0e6abc4fba6c30c52697686fc4be8e1
Ayaad Chouhan <136963590+AyaadChouhan@users.noreply.github.com>
2024-10-10 00:28:00 +0530

    Update 2.js

--- Hard-Question/2.js
+++ Hard-Question/2.js
@@ -1,31 +1,30 @@
-// // Number of Boomerangs
-// A boomerang is a V-shaped sequence that is either upright or upside down. Specifically, a boomerang can be defined as: sub-array of length 3, with the first and last digits being the same and the middle digit being different.
+// Word to Bitstring to Boolean Array
+// Create a function that converts a word to a bitstring and then to a boolean array based on the following criteria:
 
-// Some boomerang examples:
-
-// [3, 7, 3], [1, -1, 1], [5, 6, 5]
-// Create a function that returns the total number of boomerangs in an array.
-
-// To illustrate:
-
-// [3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2]
-// // 3 boomerangs in this sequence:  [3, 7, 3], [1, 5, 1], [2, -2, 2]
-// Be aware that boomerangs can overlap, like so:
-
-// [1, 7, 1, 7, 1, 7, 1]
-// // 5 boomerangs (from left to right): [1, 7, 1], [7, 1, 7], [1, 7, 1], [7, 1, 7], and [1, 7, 1]
+// Locate the position of the letter in the English alphabet (from 1 to 26).
+// Odd positions will be represented as 1 and even positions will be represented as 0.
+// Convert the represented positions to boolean values, 1 for true and 0 for false.
+// Store the conversions into an array.
 // Examples
-// countBoomerangs([9, 5, 9, 5, 1, 1, 1]) ➞ 2
+// toBoolArray("deep") ➞ [false, true, true, false]
+// deep converts to 0110
+// d is the 4th alphabet - 0
+// e is the 5th alphabet - 1
+// e is the 5th alphabet - 1
+// p is the 16th alphabet - 0
 
-// countBoomerangs([5, 6, 6, 7, 6, 3, 9]) ➞ 1
+// toBoolArray("loves") ➞ [false, true, false, true, true]
 
-// countBoomerangs([4, 4, 4, 9, 9, 9, 9]) ➞ 0
+// toBoolArray("tesh") ➞ [false, true,
 
-
-function countBoomerangs(arr) {
-    let count = 0;
-    
+function toBoolArray(val) {
+  const newArr = [];
+  for (let i = 0; i < val.length; i++) {
+    if (val[i].charCodeAt(val[i]) % 2 === 0) {
+      newArr.push(false);
+    } else newArr.push(true);
+  }
+  return newArr;
 }
 
-console.log(countBoomerangs([5, 6, 6 , 7, 6, 3, 9]))
-console.log(countBoomerangs([9, 5, 9, 5, 1, 1, 1]))
+console.log(toBoolArray("loves"));
